d25c2340bfd584f32befdc01b6e470dde3e6fdb1
Natalia Munoz Delaveau <nataliamunozdelaveau@MacBook-Pro-de-Natalia.local>
2022-12-11 17:26:34 -0300

    ESTABLEEEEEEEEEE

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/main.scss';
 
 Vue.use(BootstrapVue)
