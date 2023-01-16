4315309d7bffe4816ebf635df867b14203ff66be
Natalia Munoz Delaveau <nataliamunozdelaveau@MacBook-Pro-de-Natalia.local>
2023-01-16 18:05:01 -0300

    Estable 16/01/2023 - funciona xfi -4

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log("RUNING:", app);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
+const port = process.env.PORT || 8080
+
+app.listen(port)
+console.log("Testing-Runing:", app);
